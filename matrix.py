5b80003cb53b56f23f54af8ccb139401e1bbb8ac
Daniyal Ahmed <daniahmedkhatri@hotmail.com>
2019-09-10 13:22:52 +0500

    matrix.py added

---
+++ matrix.py
@@ -0,0 +1,30 @@
+# Author @dan
+# facebook : /whodanyalahmed
+# twitter : /whodanyalahmed
+# instagram : @whodanyalahmed
+# linkedin : /in/whodanyalahmed
+import math,numpy,pandas
+
+while True:
+    print("\n\t\tMatrix")
+    print("\t\t------------")
+    print("1 - Multiplication of Matrix")
+    print("2 - Transpose of Matrix")
+    print("E - Exit")
+
+    op = input("Enter the option: ")
+
+
+if (op == "1"):
+    matrix = [[],[],[]]
+    r = eval(input("Enter number of rows"))
+    c = eval(input("Enter number of columns"))
+    if (r == c):
+        for i in range(c):
+            col = eval(input("Enter C"+str(i) + " : "))
+
+        
+    else:
+        print("This cant be multiply")
+
+
